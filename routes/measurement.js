862ca642fcaa7104a5c179a9da0dc0546bb48eec
Pedro Martins <mpe@estg.ipvc.pt>
2020-07-23 11:51:49 +0100

    oi

--- routes/measurement.js
+++ routes/measurement.js
@@ -1,5 +1,6 @@
 const errors = require('restify-errors');
 //const Measurement = require('../models/MeasurementSet');
+const axios = require('axios');
 
 const xlsxFile = require('read-excel-file/node');
 
@@ -10,6 +11,16 @@ server.post('/excel', async(req,res,next) => {
         console.log(rows);
         console.table(rows);
        })
+});
+
+server.get('/meteo', async(req,res,next) => {
+    axios.get('https://prociv.cm-viana-castelo.pt/arcgis/rest/services/Meteo/MeteoCMVC/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=OBJECTID%20ASC&resultOffset=0&resultRecordCount=5')
+  .then(response => {
+    res.json(response.data.features[0].attributes);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 });
 
 }
